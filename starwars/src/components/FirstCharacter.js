b4ecb0fd67c9b3c0c049769b94fd38d00c52cdc7
Your Name <you@example.com>
2019-07-19 14:48:06 -0400

    got info of the first character displayed on app

--- starwars/src/components/FirstCharacter.js
+++ starwars/src/components/FirstCharacter.js
@@ -5,19 +5,30 @@ import axios from "axios";
 function FirstCharacter(data){
     console.log('here is the first character', data.data)
     const [name, setName]  = useState([]);
+    const [height, setHeight]  = useState([]);
+    const [birthyear, setBirthyear]  = useState([]);
     useEffect(()=>{
         axios
             .get(`${data.data}`)
             .then(response =>{
-            console.log('new data', response)
+            // console.log('new data', response)
             const name = response.data.name
-            console.log(name)
+            // console.log(name)
+            const height = response.data.height
+            const birthyear = response.data.birth_year
             setName(name)
+            setHeight(height)
+            setBirthyear(birthyear)
             })
     },[data])
 
     return(
-        <div>{name}</div>
+        <div>
+            <h3>Name: {name}</h3>
+            <p>Height: {height}</p>
+            <p>Birthyear: {birthyear}</p>
+        </div>
+
     );
 }
 
