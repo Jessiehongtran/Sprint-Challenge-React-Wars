9614cffe235d7ab5655f72d5b671035788ba1f2d
Your Name <you@example.com>
2019-07-19 13:42:12 -0400

    mapped over the list

--- starwars/src/components/Container.js
+++ starwars/src/components/Container.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 import Bio from "./CharacterBio";
 import Appearance from "./CharacterAppearance";
 
 
 
 function Container(){
+    const [firstCharacter, setFirstCharacter]  = useState([]);
+    const [secondCharacter, setSecondCharacter]  = useState([]);
+    const [thirdCharacter, setThirdCharacter] = useState([]);
+
+    useEffect(()=>{
+    axios
+        .get(`https://swapi.co/api/films/1/`)
+        .then(response =>{
+            // console.log('here', response)
+            const characters = response.data.characters.map(person => person)
+            // console.log('characters', characters)
+            const individual = characters.map(person => person)
+            // console.log('firstcharacter',individual[0])
+            
+            setFirstCharacter(individual[0])
+            setSecondCharacter(individual[1])
+            setThirdCharacter(individual[2])
+        })
+        .catch(err=>{
+            console.log('Got some errors', err)
+        })
+
+    }, []);
+
+
 
     return(
         <div></div>
