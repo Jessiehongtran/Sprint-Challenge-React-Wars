a02150972ba3d2a32397a40205808833edb6e0f1
Your Name <you@example.com>
2019-07-19 13:10:08 -0400

    created Components, exported & imported them

---
+++ starwars/src/components/CharacterAppearance.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+
+function Appearance(){
+    return (
+        <div></div>
+    )
+}
+
+export default Appearance;
---
+++ starwars/src/components/Container.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Bio from "./CharacterBio";
+import Appearance from "./CharacterAppearance";
+
+
+
+function Container(){
+
+    return(
+        <div></div>
+    );
+}
+
+export default Container
